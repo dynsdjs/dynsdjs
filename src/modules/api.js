46cd47953b1f0f94f1c5b93315d5e5828c449ed2
Julian Xhokaxhiu <xhokaxhiujulian@gmail.com>
2017-09-10 18:00:41 +0200

    Reduce export scope
    Only required methods and properties

--- src/modules/api.js
+++ src/modules/api.js
@@ -1,74 +1,68 @@
 import restify from 'restify'
 
+const port = process.env.HTTPPORT || 80,
+      stats = {
+        'clients': {},
+        'internalEntries': 0,
+        'totalHits': 0
+      }
+
+function countHit( currentQuestion, req, res ) {
+  if ( !( req.address.address in stats.clients ) )
+    stats.clients[ req.address.address ] = {}
+
+  if ( !( currentQuestion.name in stats.clients[ req.address.address ] ) )
+    stats.clients[ req.address.address ][ currentQuestion.name ] = { hit: 0 }
+
+  stats.clients[ req.address.address ][ currentQuestion.name ].hit++
+}
+
+function start( entries ) {
+  const server = restify.createServer()
+
+  server
+    .get(
+      '/api',
+      ( req, res, next ) => {
+        entries.keys(
+          ( err, keys ) => {
+            // Set it globally
+            stats.internalEntries = keys.length
+
+            // Start the local response logic
+            let ret = Object.assign( {}, stats )
+
+            Object.keys( stats.clients)
+              .forEach(
+                ( client ) => {
+                  Object.keys( stats.clients[ client ] )
+                    .forEach(
+                      domain => ret.totalHits += stats.clients[ client ][ domain ].hit
+                    )
+                }
+              )
+
+            res.send( ret )
+            next()
+          }
+        )
+      }
+    )
+
+  server
+    .on( 'error', e => reject( e.message ) )
+    .listen( port, () => {
+      console.log( `>> HTTP Port listening on: ${server.url}` )
+    })
+}
+
 export default class {
   constructor( dns ) {
-    const me = this
-
-    this._port = process.env.HTTPPORT || 80
-
-    this._stats = {
-      'clients': {},
-      'internalEntries': 0,
-      'totalHits': 0
-    }
-
     dns
       .on( 'init', ( entries ) => {
-        me._start( entries )
+        start( entries )
       })
-      .on( 'resolve.internal', ( currentQuestion, req, res ) => me._countHit( currentQuestion, req, res ) )
-      .on( 'resolve.external', ( currentQuestion, req, res ) => me._countHit( currentQuestion, req, res ) )
-  }
-
-  _countHit( currentQuestion, req, res ) {
-    const me = this
-
-    if ( !( req.address.address in me._stats.clients ) )
-      me._stats.clients[ req.address.address ] = {}
-
-    if ( !( currentQuestion.name in me._stats.clients[ req.address.address ] ) )
-      me._stats.clients[ req.address.address ][ currentQuestion.name ] = { hit: 0 }
-
-    me._stats.clients[ req.address.address ][ currentQuestion.name ].hit++
-  }
-
-  _start( entries ) {
-    const me = this,
-          server = restify.createServer()
-
-    server
-      .get(
-        '/api',
-        ( req, res, next ) => {
-          entries.keys(
-            ( err, keys ) => {
-              // Set it globally
-              me._stats.internalEntries = keys.length
-
-              // Start the local response logic
-              let ret = Object.assign( {}, me._stats )
-
-              Object.keys( me._stats.clients)
-                .forEach(
-                  ( client ) => {
-                    Object.keys( me._stats.clients[ client ] )
-                      .forEach(
-                        domain => ret.totalHits += me._stats.clients[ client ][ domain ].hit
-                      )
-                  }
-                )
-
-              res.send( ret )
-              next()
-            }
-          )
-        }
-      )
-
-    server
-      .on( 'error', e => reject( e.message ) )
-      .listen( me._port, () => {
-        console.log( `>> HTTP Port listening on: ${server.url}` )
-      })
+      .on( 'resolve.internal', ( currentQuestion, req, res ) => countHit( currentQuestion, req, res ) )
+      .on( 'resolve.external', ( currentQuestion, req, res ) => countHit( currentQuestion, req, res ) )
   }
 }
--- src/modules/dns.js
+++ src/modules/dns.js
@@ -3,35 +3,106 @@ import ip from 'ip'
 import NodeCache from 'node-cache'
 import EventEmitter from 'events'
 
+// Configurations
+const port = process.env.DNSPORT || 53,
+      resolvers = {
+        ipv4: [
+          process.env.DNSALT1 || '8.8.8.8',
+          process.env.DNSALT2 || '8.8.4.4'
+        ],
+        ipv6: [
+          process.env.DNS6ALT1 || '2001:4860:4860::8888',
+          process.env.DNS6ALT1 || '2001:4860:4860::8844'
+        ]
+      },
+      entries = new NodeCache({
+        // From <https://www.npmjs.com/package/node-cache>
+        // "You should set false if you want to save mutable objects or other complex types with mutability involved and wanted."
+        useClones: false
+      }),
+      // Server Handlers
+      tcpServer  = dns.createTCPServer(),
+      udp4Server = dns.createServer( { dgram_type: { type: 'udp4', reuseAddr: true } } ),
+      udp6Server = dns.createServer( { dgram_type: { type: 'udp6', reuseAddr: true } } )
+
+function request( me, req, res ) {
+  const promises = []
+
+  req.question.forEach( function ( question ) {
+    const entry = entries.get( question.name )
+
+    if ( entry ) {
+      me.emit( 'resolve.internal', question, req, res )
+
+      if ( entry.address )
+        res
+          .answer
+          .push(
+            dns.A({
+              name: entry.name || question.name,
+              address: entry.address || ip.address( 'private', 'ipv4' ),
+              ttl: entry.ttl || 600
+            })
+          )
+
+      if ( entry.address6 )
+        res
+          .answer
+          .push(
+            dns.AAAA({
+              name: entry.name || question.name,
+              address: entry.address6 || ip.address( 'private', 'ipv6' ),
+              ttl: entry.ttl || 600
+            })
+          )
+    } else {
+      me.emit( 'resolve.external', question, req, res )
+
+      promises.push(
+        recurse( me, question, req, res )
+      )
+    }
+  })
+
+  Promise
+    .all( promises )
+    .then( () => res.send() )
+    .catch( err => console.log( err ) )
+}
+
+function recurse( me, question, req, res ) {
+  const ipFamily = req.address.family.toLowerCase(),
+        resolver = resolvers[ ipFamily ][ Math.round( Math.random() ) ]
+
+  return new Promise(
+    ( resolve, reject ) => {
+      dns
+        .Request({
+          question: question,
+          server: {
+            address: resolver,
+            'port': 53,
+            'type': 'udp'
+          },
+          timeout: 1000
+        })
+        .on( 'timeout', () => reject( `>> DNS: Recursive question '${question.name}' went in timeout with resolver '${resolver}:53'` ) )
+        .on( 'message',
+          (err, msg) => {
+            msg.answer
+              .forEach( answer => res.answer.push( answer ) )
+          }
+        )
+        .on( 'end', resolve )
+        .send()
+    }
+  )
+}
+
 export default class Dns extends EventEmitter {
   constructor() {
     // Inherit EventEmitter methods
     super()
-
-    // Configurations
-    this._port = process.env.DNSPORT || 53
-
-    this._resolvers = {
-      ipv4: [
-        process.env.DNSALT1 || '8.8.8.8',
-        process.env.DNSALT2 || '8.8.4.4'
-      ],
-      ipv6: [
-        process.env.DNS6ALT1 || '2001:4860:4860::8888',
-        process.env.DNS6ALT1 || '2001:4860:4860::8844'
-      ]
-    }
-
-    this._entries = new NodeCache({
-      // From <https://www.npmjs.com/package/node-cache>
-      // "You should set false if you want to save mutable objects or other complex types with mutability involved and wanted."
-      useClones: false
-    })
-
-    // Server Handlers
-    this._tcpServer  = dns.createTCPServer()
-    this._udp4Server = dns.createServer( { dgram_type: { type: 'udp4', reuseAddr: true } } )
-    this._udp6Server = dns.createServer( { dgram_type: { type: 'udp6', reuseAddr: true } } )
   }
 
   start() {
@@ -43,14 +114,14 @@ export default class Dns extends EventEmitter {
         () => {
           return new Promise (
             ( resolve, reject ) => {
-              me._tcpServer
+              tcpServer
                 .on( 'socketError', ( e ) => reject( `[DNS] TCP: ${e.message}` ) )
-                .on( 'request', ( req, res ) => me._request( req, res ) )
+                .on( 'request', ( req, res ) => request( me, req, res ) )
                 .on( 'listening', () => {
                   console.log( `>> DNS: Listening on [::]:53/tcp` )
                   resolve()
                 })
-                .serve( me._port )
+                .serve( port )
             }
           )
         }
@@ -59,14 +130,14 @@ export default class Dns extends EventEmitter {
         () => {
           return new Promise (
             ( resolve, reject ) => {
-              me._udp4Server
+              udp4Server
                 .on( 'socketError', ( e ) => reject( `[DNS] UDP4: ${e.message}` ) )
-                .on( 'request', ( req, res ) => me._request( req, res ) )
+                .on( 'request', ( req, res ) => request( me, req, res ) )
                 .on( 'listening', () => {
                   console.log( `>> DNS: Listening on 0.0.0.0:53/udp` )
                   resolve()
                 })
-                .serve( me._port )
+                .serve( port )
             }
           )
         }
@@ -75,96 +146,20 @@ export default class Dns extends EventEmitter {
         () => {
           return new Promise (
             ( resolve, reject ) => {
-              me._udp6Server
+              udp6Server
                 .on( 'socketError', ( e ) => reject( `[DNS] UDP6: ${e.message}` ) )
-                .on( 'request', ( req, res ) => me._request( req, res ) )
+                .on( 'request', ( req, res ) => request( me, req, res ) )
                 .on( 'listening', () => {
                   console.log( `>> DNS: Listening on [::]:53/udp` )
                   resolve()
                 })
-                .serve( me._port )
+                .serve( port )
             }
           )
         }
       )
       .then(
-        () => this.emit( 'init', this._entries )
+        () => me.emit( 'init', entries )
       )
-  }
-
-  _request( req, res ) {
-    const me = this,
-          promises = []
-
-    req.question.forEach( function ( question ) {
-      const entry = me._entries.get( question.name )
-
-      if ( entry ) {
-        me.emit( 'resolve.internal', question, req, res )
-
-        if ( entry.address )
-          res
-            .answer
-            .push(
-              dns.A({
-                name: entry.name || question.name,
-                address: entry.address || ip.address( 'private', 'ipv4' ),
-                ttl: entry.ttl || 600
-              })
-            )
-
-        if ( entry.address6 )
-          res
-            .answer
-            .push(
-              dns.AAAA({
-                name: entry.name || question.name,
-                address: entry.address6 || ip.address( 'private', 'ipv6' ),
-                ttl: entry.ttl || 600
-              })
-            )
-      } else {
-        me.emit( 'resolve.external', question, req, res )
-
-        promises.push(
-          me._recurse( question, req, res )
-        )
-      }
-    })
-
-    Promise
-      .all( promises )
-      .then( () => res.send() )
-      .catch( err => console.log( err ) )
-  }
-
-  _recurse( question, req, res ) {
-    const me = this,
-          ipFamily = req.address.family.toLowerCase(),
-          resolver = me._resolvers[ ipFamily ][ Math.round( Math.random() ) ]
-
-    return new Promise(
-      ( resolve, reject ) => {
-        dns
-          .Request({
-            question: question,
-            server: {
-              address: resolver,
-              'port': 53,
-              'type': 'udp'
-            },
-            timeout: 1000
-          })
-          .on( 'timeout', () => reject( `>> DNS: Recursive question '${question.name}' went in timeout with resolver '${resolver}:53'` ) )
-          .on( 'message',
-            (err, msg) => {
-              msg.answer
-                .forEach( answer => res.answer.push( answer ) )
-            }
-          )
-          .on( 'end', resolve )
-          .send()
-      }
-    )
   }
 }
