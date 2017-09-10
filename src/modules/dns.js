28c821439c05d5bb5cb04755413b4e38b5b0b98a
Julian Xhokaxhiu <xhokaxhiujulian@gmail.com>
2017-09-10 16:06:11 +0200

    DNS Handle code as Class
    Much easier to handle and write

--- src/modules/dns.js
+++ src/modules/dns.js
@@ -3,94 +3,101 @@ import fetch from 'node-fetch'
 import fs from 'fs'
 import ip from 'ip'
 
-let dnsConfig
-
-function proxyDnsRequest ( ipType, question, response ) {
-  const resolver = dnsConfig.dns.resolver[ ipType ][ Math.round( Math.random() ) ]
-
-  return new Promise(
-    ( resolve, reject ) => {
-      dns
-        .Request({
-          question: question,
-          server: {
-            address: resolver,
-            'port': 53,
-            'type': 'udp'
-          },
-          timeout: 1000
-        })
-        .on( 'timeout', () => reject( `>> DNS: Recursive question '${question.name}' went in timeout with resolver '${resolver}:53'` ) )
-        .on( 'message',
-          (err, msg) => {
-            msg.answer
-              .forEach( answer => response.answer.push( answer ) )
-          }
-        )
-        .on( 'end', resolve )
-        .send()
-    }
-  )
-}
-
-function answerDnsRequest ( req, res ) {
-  let promises = []
-
-  if ( !( req.address.address in dnsConfig.http.stats.clients ) ) {
-    dnsConfig.http.stats.clients[ req.address.address ] = {
-      'ads': 0,
-      'generic': 0
-    }
+class DnsHandler {
+  constructor( config ) {
+    this.config = config
   }
 
-  req.question.forEach( function ( question ) {
-    const adDomain = dnsConfig.dns.cache.get( question.name )
+  request( req, res ) {
+    const me = this,
+          promises = []
 
-    if ( adDomain ) {
-      res
-        .answer
-        .push(
-          dns.A({
-            name: question.name,
-            address: ip.address( 'private', 'ipv4' ),
-            ttl: 600
-          })
-        )
+    me.res = res
 
-      res
-        .answer
-        .push(
-          dns.AAAA({
-            name: question.name,
-            address: ip.address( 'private', 'ipv6' ),
-            ttl: 600
-          })
-        )
-
-      dnsConfig.http.stats.clients[ req.address.address ].ads++
-
-      dnsConfig.dns.cache.set( question.name, { hit: ++adDomain.hit } )
-    } else {
-      dnsConfig.http.stats.clients[ req.address.address ].generic++
-
-      promises.push(
-        proxyDnsRequest( req.address.family.toLowerCase(), question, res )
-      )
+    if ( !( req.address.address in me.config.http.stats.clients ) ) {
+      me.config.http.stats.clients[ req.address.address ] = {
+        'ads': 0,
+        'generic': 0
+      }
     }
-  })
 
-  Promise
-    .all( promises )
-    .then( () => res.send() )
-    .catch( err => console.log( err ) )
+    req.question.forEach( function ( question ) {
+      const adDomain = me.config.dns.cache.get( question.name )
+
+      if ( adDomain ) {
+        res
+          .answer
+          .push(
+            dns.A({
+              name: question.name,
+              address: ip.address( 'private', 'ipv4' ),
+              ttl: 600
+            })
+          )
+
+        res
+          .answer
+          .push(
+            dns.AAAA({
+              name: question.name,
+              address: ip.address( 'private', 'ipv6' ),
+              ttl: 600
+            })
+          )
+
+        me.config.http.stats.clients[ req.address.address ].ads++
+
+        me.config.dns.cache.set( question.name, { hit: ++adDomain.hit } )
+      } else {
+        me.config.http.stats.clients[ req.address.address ].generic++
+
+        promises.push(
+          me._recurse( req.address.family.toLowerCase(), question )
+        )
+      }
+    })
+
+    Promise
+      .all( promises )
+      .then( () => res.send() )
+      .catch( err => console.log( err ) )
+  }
+
+  _recurse( ip, question ) {
+    const me = this,
+          resolver = me.config.dns.resolver[ ip ][ Math.round( Math.random() ) ]
+
+    return new Promise(
+      ( resolve, reject ) => {
+        dns
+          .Request({
+            question: question,
+            server: {
+              address: resolver,
+              'port': 53,
+              'type': 'udp'
+            },
+            timeout: 1000
+          })
+          .on( 'timeout', () => reject( `>> DNS: Recursive question '${question.name}' went in timeout with resolver '${resolver}:53'` ) )
+          .on( 'message',
+            (err, msg) => {
+              msg.answer
+                .forEach( answer => me.res.answer.push( answer ) )
+            }
+          )
+          .on( 'end', resolve )
+          .send()
+      }
+    )
+  }
 }
 
 export default ( config ) => {
   const udp4Server = dns.createServer( { dgram_type: { type: 'udp4', reuseAddr: true } } ),
         udp6Server = dns.createServer( { dgram_type: { type: 'udp6', reuseAddr: true } } ),
-        tcpServer  = dns.createTCPServer()
-
-  dnsConfig = config
+        tcpServer  = dns.createTCPServer(),
+        dnsHandler = new DnsHandler( config )
 
   return new Promise (
     ( resolve, reject ) => {
@@ -102,12 +109,12 @@ export default ( config ) => {
               ( resolve, reject ) => {
                 tcpServer
                   .on( 'socketError', ( e ) => reject( `[DNS] TCP: ${e.message}` ) )
-                  .on( 'request', answerDnsRequest )
+                  .on( 'request', ( req, res ) => dnsHandler.request( req, res ) )
                   .on( 'listening', () => {
                     console.log( `>> DNS: Listening on [::]:53/tcp` )
                     resolve()
                   })
-                  .serve( dnsConfig.dns.port )
+                  .serve( config.dns.port )
               }
             )
           }
@@ -118,12 +125,12 @@ export default ( config ) => {
               ( resolve, reject ) => {
                 udp4Server
                   .on( 'socketError', ( e ) => reject( `[DNS] UDP4: ${e.message}` ) )
-                  .on( 'request', answerDnsRequest )
+                  .on( 'request', ( req, res ) => dnsHandler.request( req, res ) )
                   .on( 'listening', () => {
                     console.log( `>> DNS: Listening on 0.0.0.0:53/udp` )
                     resolve()
                   })
-                  .serve( dnsConfig.dns.port )
+                  .serve( config.dns.port )
               }
             )
           }
@@ -134,12 +141,12 @@ export default ( config ) => {
               ( resolve, reject ) => {
                 udp6Server
                   .on( 'socketError', ( e ) => reject( `[DNS] UDP6: ${e.message}` ) )
-                  .on( 'request', answerDnsRequest )
+                  .on( 'request', ( req, res ) => dnsHandler.request( req, res ) )
                   .on( 'listening', () => {
                     console.log( `>> DNS: Listening on [::]:53/udp` )
                     resolve()
                   })
-                  .serve( dnsConfig.dns.port )
+                  .serve( config.dns.port )
               }
             )
           }
