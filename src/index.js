8ff63c95a2427e7d77e097bf30e5adde2a33df77
Julian Xhokaxhiu <xhokaxhiujulian@gmail.com>
2017-09-10 19:16:18 +0200

    Introduce the new Plugin logic
    index.js will not hold the code to load plugins installed locally to this package instance.
    As we will not have anything else other than the DNS code, we also remove the modules folder and place dns.js inside the src/ folder.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,32 @@
-import Api from './modules/api'
-import Dns from './modules/dns'
+import glob from 'glob'
+import path from 'path'
+import Dns from './dns'
+import { name as packageName } from '../package.json'
 
 export default () => {
   const dns = new Dns()
 
   // Load Plugins
-  new Api( dns )
+  glob(
+    `./node_modules/${packageName}-plugin-*`,
+    ( err, files ) => {
+        files
+          .forEach(
+            ( plugin ) => {
+              plugin = path.basename( plugin )
 
-  dns.start()
+              const instance = eval('require')( plugin )
+
+              if ( instance && instance.default )
+                new instance.default( dns )
+              else if ( instance ) {
+                new instance( dns )
+              }
+            }
+          )
+
+        // Finally start the DNS daemon
+        dns.start()
+    }
+  )
 }
