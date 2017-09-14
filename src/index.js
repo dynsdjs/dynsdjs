980fbb4f7f5ca3613a01182d5338d6eae7a98e04
Julian Xhokaxhiu <xhokaxhiujulian@gmail.com>
2017-09-14 23:30:30 +0200

    Allow loading of local and global plugins

--- src/index.js
+++ src/index.js
@@ -1,32 +1,73 @@
+import { exec } from 'child_process'
 import glob from 'glob'
 import path from 'path'
 import Dns from './dns'
 import { name as packageName } from '../package.json'
 
-export default () => {
-  const dns = new Dns()
+const dns = new Dns()
 
-  // Load Plugins
-  glob(
-    `./node_modules/${packageName}-plugin-*`,
-    ( err, files ) => {
-        files
-          .forEach(
-            ( plugin ) => {
-              plugin = path.basename( plugin )
-
-              const instance = eval('require')( plugin )
-
-              if ( instance && instance.default )
-                new instance.default( dns )
-              else if ( instance ) {
-                new instance( dns )
-              }
-            }
-          )
-
-        // Finally start the DNS daemon
-        dns.start()
+function getNodeModulesPath( isGlobal ) {
+  return new Promise (
+    ( resolve, reject ) => {
+      if ( isGlobal )
+        exec(
+          `npm config get prefix -g`,
+          ( err, stdout, stderr ) => {
+            if ( err ) reject( err )
+            else if ( stderr ) reject ( stderr )
+            else resolve( stdout )
+          }
+        )
+      else
+        resolve(
+          `${path.resolve()}/node_modules`
+        )
     }
   )
+}
+
+function loadPlugins( pluginPath ) {
+  return new Promise (
+    ( resolve, reject ) => {
+      // Load Plugins
+      glob(
+        `${pluginPath}/${packageName}-plugin-*`,
+        ( err, files ) => {
+            if ( err ) reject( err )
+            else {
+              files
+                .forEach(
+                  ( plugin ) => {
+                    plugin = path.basename( plugin )
+
+                    const instance = eval('require')( plugin )
+
+                    if ( instance && instance.default )
+                      new instance.default( dns )
+                    else if ( instance ) {
+                      new instance( dns )
+                    }
+                  }
+                )
+
+              // Finally resolve
+              resolve()
+            }
+        }
+      )
+    }
+  )
+}
+
+export default () => {
+  Promise.resolve()
+    .catch( err => console.log( err ) )
+    // Load global plugins
+    .then( () => getNodeModulesPath( true ) )
+    .then( path => loadPlugins( path ) )
+    // Load local plugins
+    .then( () => getNodeModulesPath() )
+    .then( path => loadPlugins( path ) )
+    // Start the DNS
+    .then( () => dns.start() )
 }
