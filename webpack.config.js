b07074c46caee954d44bf42b3d5abd31118b2ef8
Julian Xhokaxhiu <xhokaxhiujulian@gmail.com>
2017-11-01 18:38:35 +0100

    Enhance the development
    Provide sourcemap support for crashes. This helps a lot during development of plugins.
    In order to enable the sourcemap support, the daemon MUST be built using webpack development profile.
    Also, in order to easy the maintenance of the webpack.config.js file, we merged everything back to one file.

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,57 @@
-module.exports = function( env ) {
-  // Default to dev if no environment given
-  if ( !env ) env = 'dev'
+const path = require('path'),
+      webpack = require('webpack'),
+      BabiliPlugin = require('babili-webpack-plugin'),
+      WebpackSourceMapSupport = require('webpack-source-map-support')
 
-  return require(`./webpack.${env}.js`)
+module.exports = function( env ) {
+  let config = {
+    entry: './src/',
+    target: 'node',
+    node: {
+      __dirname: false,
+      __filename: false,
+    },
+    stats: {
+      warnings: false
+    },
+    resolve: {
+      extensions: [ '.js' ]
+    },
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'Dynsd.js',
+      library: 'Dynsd',
+      libraryTarget: 'umd2'
+    },
+    plugins: []
+  }
+
+  if ( !env ) {
+    // ---- DEV ----
+    config.devtool = 'inline-source-map'
+
+    Array.prototype.push.apply(
+      config.plugins,
+      [
+        new WebpackSourceMapSupport()
+      ]
+    )
+  } else {
+    // ---- PROD ----
+    Array.prototype.push.apply(
+      config.plugins,
+      [
+        new webpack.LoaderOptionsPlugin({
+          minimize: true,
+          debug: false
+        }),
+        new BabiliPlugin({}, {
+          comments: false,
+          sourceMap: false
+        })
+      ]
+    )
+  }
+
+  return config
 }
